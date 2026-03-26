eebf760b48c483e2142f79e759dbdfec44043330
Wendisw <Wendi.s.wijaya@danamas.co.id>
2026-03-26 19:23:55 +0700

    fix env

--- admin-commerce/src/environments/environment.prod.ts
+++ admin-commerce/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-api-domain.com/api/v1',
-  adminApiUrl: 'https://your-api-domain.com/api/v1/admin',
+  apiUrl: 'https://api.tanpatisserie.com/api/v1',
+  adminApiUrl: 'https://api.tanpatisserie.com/api/v1/admin',
 };
